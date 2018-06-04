a2852fdec367d724d520ea4e90887cf1fcbd1e33
reymont <258227346@qq.com>
2018-06-04 15:13:36 +0800

    greeter

--- src/main/java/chap7/ch7_2/Greeter.java
+++ src/main/java/chap7/ch7_2/Greeter.java
@@ -1,4 +1,4 @@
-package chap7;
+package chap7.ch7_2;
 
 import akka.actor.UntypedActor;
 
--- src/main/java/chap7/ch7_2/HelloWorld.java
+++ src/main/java/chap7/ch7_2/HelloWorld.java
@@ -1,4 +1,4 @@
-package chap7;
+package chap7.ch7_2;
 
 import akka.actor.ActorRef;
 import akka.actor.Props;
